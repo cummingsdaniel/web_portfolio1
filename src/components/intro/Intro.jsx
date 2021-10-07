1d181cf0a66f20c2cc900e62be4ac0480cdefe54
danstonmurphy <cumm0082@algonquinlive.com>
2021-10-07 13:23:38 -0400

    Thursday 7th first changes

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -5,10 +5,19 @@ export default function Intro() {
         <div className="intro" id="intro">
             <div className="left">
                 <div className="imgContainer">
-                    <img src="assest/logo_black.png" alt=""/>
+                    <img src="assest/selfpic.png" alt=""/>
                 </div>
             </div>
-            <div className="right"></div>
+            <div className="right">
+                <div className="wrapper">
+                    <h2>Hello, I'm</h2>
+                    <h1>Daniel Cummings</h1>
+                    <h3>Freelance <span>Software Developer | Web Developer | Designer</span></h3>
+                </div>
+                <a href="#portfolio">
+                    <img src="assest/arrow.png" alt="" />
+                </a>
+            </div>
         </div>
     )
 }
