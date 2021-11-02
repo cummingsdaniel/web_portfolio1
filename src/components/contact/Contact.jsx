b0298db33af336ec05379688a59472f590098029
danstonmurphy <cumm0082@algonquinlive.com>
2021-11-02 17:11:26 -0400

    form 1

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -14,20 +14,22 @@ export default function Contact() {
             </div>
             <div className="right">
                 <h2>Contact.</h2>
-                <form name="contact v1" action="post" data-netlify="true" onSubmit="submit">
+                <form name="contact v1"
+                      action="post" 
+                      data-netlify="true" 
+                      onSubmit="submit"
+                      data-netlifty-honeypot="bot-field">
                     
                     <input type="hidden" name="form-name" value="contact v1"/>
-                    
-                    
+                    <div hidden>
+                        <input name="bot-field" />
+                    </div>    
                     <input type="text" name="first-name" placeholder="First Name" />
                     
                     <input type="email" name="email" placeholder="Email" />
                     
                     <textarea name="message" placeholder="Message"></textarea>
                     
-                    <div className="field">
-                        <div data-netlify-recaptcha="true"></div>
-                    </div>
                     <button type="submit">Send</button>
                     {/* {message && <span>Thanks, I'll reply ASAP :) </span>} */}
                 </form>
