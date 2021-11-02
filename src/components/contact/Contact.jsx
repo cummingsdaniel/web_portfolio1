1665af6b19dc767d483920852215b4c724eb81b1
danstonmurphy <cumm0082@algonquinlive.com>
2021-11-02 16:39:41 -0400

    contact 3

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,13 +1,13 @@
 import "./contact.scss"
-import { useState } from "react";
+// import { useState } from "react";
 
 export default function Contact() {
-    const [message, setMessage] = useState(false);
+    // const [message, setMessage] = useState(false);
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        setMessage(true);
-    };  
+    // const handleSubmit = (e) => {
+    //     e.preventDefault();
+    //     setMessage(true);
+    // };  
     return (
         <div className="contact" id="contact">
             <div className="left">            
